cb007ea039db30c3325f5a85725dea883266b7bf
adity <adityaahmadnugraha091@gmail.com>
2023-09-23 22:22:37 +0700

    Initial commit

---
+++ app/src/main/java/com/aditya/appsatipadang/ui/splash/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.aditya.appsatipadang.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import com.aditya.appsatipadang.MainActivity
+import com.aditya.appsatipadang.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        Handler(Looper.getMainLooper()).postDelayed({
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1500)
+    }
+}
