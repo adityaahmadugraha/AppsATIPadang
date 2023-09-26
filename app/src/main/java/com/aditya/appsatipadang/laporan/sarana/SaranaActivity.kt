11e4d1752463fafae40e1eab200aafdc9a1e5322
adity <adityaahmadnugraha091@gmail.com>
2023-09-26 15:49:28 +0700

    camera

--- app/src/main/java/com/aditya/appsatipadang/laporan/sarana/SaranaActivity.kt
+++ app/src/main/java/com/aditya/appsatipadang/laporan/sarana/SaranaActivity.kt
@@ -1,15 +1,62 @@
 package com.aditya.appsatipadang.laporan.sarana
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.aditya.appsatipadang.MainActivity
 import com.aditya.appsatipadang.R
 import com.aditya.appsatipadang.databinding.ActivitySaranaBinding
+import com.aditya.appsatipadang.ui.camera.CameraActivity
+import com.aditya.appsatipadang.ui.camera.createCustomTempFile
+import com.aditya.appsatipadang.ui.camera.rotateFile
+import com.aditya.appsatipadang.ui.camera.uriToFile
+import java.io.File
 
 class SaranaActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySaranaBinding
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    "Tidak mendapatkan permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -17,10 +64,100 @@ class SaranaActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.hide()
-
         binding.imgBack.setOnClickListener() {
             val intent = Intent(this@SaranaActivity, MainActivity::class.java)
             startActivity(intent)
         }
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+        binding.camera.setOnClickListener { startCameraX() }
+
+
     }
+
+    private fun startCameraX() {
+        val intent = Intent(this, CameraActivity::class.java)
+        launcherIntentCameraX.launch(intent)
+    }
+
+//    private fun startTakePhoto() {
+//        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//        intent.resolveActivity(packageManager)
+//
+//        createCustomTempFile(application).also {
+//            val photoURI: Uri = FileProvider.getUriForFile(
+//                this@SaranaActivity,
+//                "com.aditya.appsatipadang",
+//                it
+//            )
+//            currentPhotoPath = it.absolutePath
+//            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+//            launcherIntentCamera.launch(intent)
+//        }
+//    }
+
+//    private fun startGallery() {
+//        val intent = Intent()
+//        intent.action = Intent.ACTION_GET_CONTENT
+//        intent.type = "image/*"
+//        val chooser = Intent.createChooser(intent, "Choose a Picture")
+//        launcherIntentGallery.launch(chooser)
+//    }
+
+    private fun uploadImage() {
+        Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+    }
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERA_X_RESULT) {
+            val myFile = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                it.data?.getSerializableExtra("picture", File::class.java)
+            } else {
+                @Suppress("DEPRECATION")
+                it.data?.getSerializableExtra("picture")
+            } as? File
+
+            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+
+            myFile?.let { file ->
+                rotateFile(file, isBackCamera)
+                binding.imgFoto.setImageBitmap(BitmapFactory.decodeFile(file.path))
+            }
+        }
+    }
+
+    private lateinit var currentPhotoPath: String
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            val myFile = File(currentPhotoPath)
+
+            myFile.let { file ->
+//              Silakan gunakan kode ini jika mengalami perubahan rotasi
+//              rotateFile(file)
+                binding.imgFoto.setImageBitmap(BitmapFactory.decodeFile(file.path))
+            }
+        }
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg = result.data?.data as Uri
+            selectedImg.let { uri ->
+                val myFile = uriToFile(uri, this@SaranaActivity)
+                binding.imgFoto.setImageURI(uri)
+            }
+        }
+    }
+
 }
